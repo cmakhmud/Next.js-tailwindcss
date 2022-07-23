6d3cb311df13b2b6182959172da7395e223f8b5f
cmakhmud <98968434+cmakhmud@users.noreply.github.com>
2022-07-23 13:48:37 +0400

    Update property.js

--- myapp/pages/property.js
+++ myapp/pages/property.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import propertyImg from '../public/assets/bg.png'
+import propertyImg from '../public/assets/pikkpro.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { RiRadioButtonFill } from 'react-icons/ri'
@@ -16,68 +16,55 @@ export default function proporty() {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Property Finder</h2>
-          <h3>React JS / Tailwind / Firebase</h3>
+          <h2 className='py-2'>Pikkpro app</h2>
+          <h3>React-Native</h3>
         </div>
       </div>
 
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
         <div className='col-span-4'>
-          <p>Project</p>
-          <h2>Overview</h2>
+          <p>Proyekt</p>
+          <h2>Haqqında</h2>
           <p>
-            This app was built using React JS and is hosted on Firebase. Users
-            are able to search properties based on an Address, City, or ZIP code
-            to retrieve a list of active properties currently for sale. You will
-            be able to view property information as well as the specific
-            location of the property integrated with the Google Maps API. User
-            authentication is available so you can signup and signin to your
-            account with an email address in order to save your favorite
-            properties. This is made possible with Zillow API.
+            Bu mobil proqramın dizayn ve front-end development hissəsi bizim komanda tərəfindən react-native istifadə olunaraq düzəldilib . 
+            Proqram haqqında qısaca məlumat versək , freelancing saytıdır . Sayta Buyer yəni alıcı və freelancer yəni Buyerlərin işin görən biri kimi daxil olmaq olar.
+            Buyerler öz təkliflərini bu app də yerləşdirir və freelancerlər müraciət edərək onların işini görür . Burdan program sahibinin qazancı isə freelancerləri va buyerləri
+            elaqələndirərək onlardan müəyyən faiz hissəsi tutur.
           </p>
-          <a
+         {/*  <a
             href='https://github.com/fireclint/property-finder'
             target='_blank'
             rel='noreferrer'
           >
             <button className='px-8 py-2 mt-4 mr-8'>Code</button>
-          </a>
-          <a
+          </a> */}
+          {/* <a
             href='https://property-finder-development.web.app/'
             target='_blank'
             rel='noreferrer'
           >
             <button className='px-8 py-2 mt-4'>Demo</button>
-          </a>
+          </a> */}
 
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
           <div className='p-2'>
-            <p className='text-center font-bold pb-2'>Technologies</p>
+            <p className='text-center font-bold pb-2'>Texnalogiyalar</p>
             <div className='grid grid-cols-3 md:grid-cols-1'>
               <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> React
+                <RiRadioButtonFill className='pr-1' /> React-native
               </p>
               <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Tailwind
+                <RiRadioButtonFill className='pr-1' /> Css
               </p>
               <p className='text-gray-600 py-2 flex items-center'>
                 <RiRadioButtonFill className='pr-1' /> Javascript
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Firebase
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Google API
-              </p>
-              <p className='text-gray-600 py-2 flex items-center'>
-                <RiRadioButtonFill className='pr-1' /> Zillow API
               </p>
             </div>
           </div>
         </div>
         <Link href='/#projects'>
-          <p className='underline cursor-pointer'>Back</p>
+          <button className='px-8 py-2 mt-4'>Back</button>
         </Link>
       </div>
     </div>

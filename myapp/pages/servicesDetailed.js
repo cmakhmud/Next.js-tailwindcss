5db47592f5be1e115c5e29a1605fb313d6082470
cmakhmud <98968434+cmakhmud@users.noreply.github.com>
2022-07-23 12:32:56 +0400

    Update servicesDetailed.js

--- myapp/pages/servicesDetailed.js
+++ myapp/pages/servicesDetailed.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import propertyImg from '../public/assets/White Background.png'
+import propertyImg from '../public/assets/bg.png'
 import {FaDesktop , FaSearchengin} from 'react-icons/fa'
 import {FcSmartphoneTablet , FcAdvertising} from 'react-icons/fc'
 import {BiTransfer} from 'react-icons/bi'

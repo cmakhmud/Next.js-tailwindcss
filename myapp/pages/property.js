86af7f09a6352883ca771e0cdda05751c7495167
cmakhmud <98968434+cmakhmud@users.noreply.github.com>
2022-07-23 12:37:32 +0400

    Update property.js

--- myapp/pages/property.js
+++ myapp/pages/property.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import propertyImg from '../public/assets/property.jpg'
+import propertyImg from '../public/assets/Purple Modern Service Coding Logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { RiRadioButtonFill } from 'react-icons/ri'
